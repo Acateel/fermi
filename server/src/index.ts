8f6ab9e79d8834c25d5cfcde8d0f501bb67dda00
Taras Kozodoi <acateely@gmail.com>
2023-03-07 15:36:48 +0200

    get port from env

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,8 @@
 import app from "./server";
+import * as dotenv from "dotenv"
+dotenv.config()
 
-const port = 3001;
+const port = process.env.PORT
 
 app.listen(port, () => {
   console.log(`server start on http://localhost:${port}`);
