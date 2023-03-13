dc79f7eeb7bf2c6423fe65655feeb87fb3bc7f25
Taras Kozodoi <acateely@gmail.com>
2023-03-13 18:54:23 +0200

    add template for login

---
+++ client/src/components/auth/Login.tsx
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+import { connect, ConnectedProps } from "react-redux";
+import { RootState } from "../../state/store";
+import { signIn } from "../../state/creators";
+
+const mapState = (state: RootState) => ({
+  auth: state.auth,
+});
+
+const connector = connect(mapState, { signIn });
+
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+const Login = (props: PropsFromRedux) => {
+  useEffect(() => {}, []);
+  return <div>Login</div>;
+};
+
+export default connector(Login);
