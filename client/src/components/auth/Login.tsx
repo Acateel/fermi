ab33a9d013d7e2824ca6993924e5c375a2cf50e1
Taras Kozodoi <acateely@gmail.com>
2023-03-14 14:11:26 +0200

    add style in login component

--- client/src/components/auth/Login.tsx
+++ client/src/components/auth/Login.tsx
@@ -3,6 +3,7 @@ import { connect, ConnectedProps } from "react-redux";
 import { RootState } from "../../state/store";
 import { signIn } from "../../state/creators";
 import { Navigate } from "react-router-dom";
+import "./Login.css";
 
 const mapState = (state: RootState) => ({
   auth: state.auth,
@@ -13,16 +14,16 @@ const connector = connect(mapState, { signIn });
 type PropsFromRedux = ConnectedProps<typeof connector>;
 
 const Login = (props: PropsFromRedux) => {
-  const [username, setUsername] = useState("")
-  const [password, setPassword] = useState("")
-  
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   const onFormSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    props.signIn(username, password)
-  }
+    props.signIn(username, password);
+  };
 
-  if(props.auth){
-    return <Navigate to="/conversation" replace={true} />
+  if (props.auth) {
+    return <Navigate to="/conversation" replace={true} />;
   }
 
   return (
