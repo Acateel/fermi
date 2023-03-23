03cb1edfcf1f1fe7e038e49523c1168af0b07de7
Taras Kozodoi <acateely@gmail.com>
2023-03-23 20:53:50 +0200

    add style for home component

--- client/src/components/home/Home.tsx
+++ client/src/components/home/Home.tsx
@@ -1,18 +1,23 @@
 import { Link } from "react-router-dom";
+import "./Home.css";
 
 const Home = () => {
-    return (
-        <div className="home">
-            <img className="home_logo" src="/fermi-logo.png"/>
-            <img className="home_logo_text" src="/fermi.png"/>
-            <Link to="/login">Login</Link>
-            <Link to="/registration">Registration</Link>
-            <footer>
-                <a href="https://github.com/Acateel/fermi">Github</a>
-                <a href="mailto:acateely@gmail.com">Feedback</a>
-            </footer>
+  return (
+    <div className="home">
+      <main>
+        <img className="home_logo" src="/fermi-logo.png" />
+        <img className="home_logo_text" src="/fermi.png" />
+        <div className="home_links">
+          <Link to="/login">Login</Link>
+          <Link to="/registration">Registration</Link>
         </div>
-    )
-}
+      </main>
+      <footer>
+        <a href="https://github.com/Acateel/fermi">Github</a>
+        <a href="mailto:acateely@gmail.com">Feedback</a>
+      </footer>
+    </div>
+  );
+};
 
 export default Home;
